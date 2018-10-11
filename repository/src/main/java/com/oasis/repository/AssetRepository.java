1e93c21ac2151aa9af5976088ebe3c58adf29468
Jonathan <jonathan16998@gmail.com>
2018-10-11 21:00:30 +0700

    Added repository classes for dashboard page
    Repository classes for asset and request have been created, with
    addition of some methods.

---
+++ repository/src/main/java/com/oasis/repository/AssetRepository.java
@@ -0,0 +1,12 @@
+package com.oasis.repository;
+
+import com.oasis.model.entity.AssetModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AssetRepository extends MongoRepository<AssetModel, String> {
+    List<AssetModel> findAllByStockGreaterThan(Integer stockLimit);
+}
---
+++ repository/src/main/java/com/oasis/repository/RequestRepository.java
@@ -0,0 +1,13 @@
+package com.oasis.repository;
+
+import com.oasis.model.entity.RequestModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RequestRepository extends MongoRepository<RequestModel, String> {
+    List<RequestModel> findAllByStatusIs(String status);
+    List<RequestModel> findAllByEmployeeIdEquals(String employeeId);
+}
