ab1b6e500efedaca340e0dc6b47bd2d1e0099dd0
Jonathan <jonathan16998@gmail.com>
2018-10-11 18:33:29 +0700

    Renamed LoginRepository to EmployeeRepository
    The repository class is renamed as the reason for namin the repository
    is incorrect. While previous commits believe that a repository class is
    specifically created for a feature, the correct reason is a repository
    should be created for a collection/table in database.

--- repository/src/main/java/com/oasis/repository/EmployeeRepository.java
+++ repository/src/main/java/com/oasis/repository/EmployeeRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LoginRepository extends MongoRepository<EmployeeModel, String> {
+public interface EmployeeRepository extends MongoRepository<EmployeeModel, String> {
     EmployeeModel findByUsername(String username);
 }
--- service/src/main/java/com/oasis/service/implementation/LoginServiceImpl.java
+++ service/src/main/java/com/oasis/service/implementation/LoginServiceImpl.java
@@ -1,7 +1,7 @@
 package com.oasis.service.implementation;
 
 import com.oasis.model.entity.EmployeeModel;
-import com.oasis.repository.LoginRepository;
+import com.oasis.repository.EmployeeRepository;
 import com.oasis.service.api.LoginServiceApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,11 +10,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoginServiceImpl implements LoginServiceApi {
     @Autowired
-    private LoginRepository loginRepository;
+    private EmployeeRepository employeeRepository;
 
     @Override
     public EmployeeModel checkLoginCredentials(String username, String password) {
-        EmployeeModel result = loginRepository.findByUsername(username);
+        EmployeeModel result = employeeRepository.findByUsername(username);
 
         if (result != null){
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
