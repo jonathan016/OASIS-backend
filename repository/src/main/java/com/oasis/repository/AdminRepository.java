28d42ab87e5a5a89f278dc60d56c554115d3ecd2
Jonathan <jonathan16998@gmail.com>
2018-10-23 08:05:04 +0700

    Added @Repository annottion in AdminRepository
    Forgot to add @Repository annotation in AdminRepository, so the
    annotation is added.

--- repository/src/main/java/com/oasis/repository/AdminRepository.java
+++ repository/src/main/java/com/oasis/repository/AdminRepository.java
@@ -2,7 +2,9 @@ package com.oasis.repository;
 
 import com.oasis.model.entity.AdminModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdminRepository extends MongoRepository<AdminModel, String> {
 
     AdminModel findByNik(String nik);
