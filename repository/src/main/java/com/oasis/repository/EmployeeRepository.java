4f40120a11e70c480e42d09ecd74bab77e5adac9
Jonathan <jonathan16998@gmail.com>
2018-10-28 15:45:45 +0700

    Added methods for viewing all employees subfeature
    Methods from repository for viewing all employees subfeature are added.

--- repository/src/main/java/com/oasis/repository/EmployeeRepository.java
+++ repository/src/main/java/com/oasis/repository/EmployeeRepository.java
@@ -4,10 +4,22 @@ import com.oasis.model.entity.EmployeeModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeRepository extends MongoRepository<EmployeeModel, String> {
 
     EmployeeModel findByUsername(String username);
 
     EmployeeModel findByNik(String nik);
+
+    List<EmployeeModel> findAllByNikContainsOrFullnameContains(String nik, String fullname);
+
+    List<EmployeeModel> findAllByNikContainsOrFullnameContainsOrderByNikAsc(String nik, String fullname);
+
+    List<EmployeeModel> findAllByNikContainsOrFullnameContainsOrderByNikDesc(String nik, String fullname);
+
+    List<EmployeeModel> findAllByNikContainsOrFullnameContainsOrderByFullnameAsc(String nik, String fullname);
+
+    List<EmployeeModel> findAllByNikContainsOrFullnameContainsOrderByFullnameDesc(String nik, String fullname);
 }
