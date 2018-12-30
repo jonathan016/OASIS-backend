6a6639ae6b2b76a7dad6734a7986e2072a4ce61f
Jonathan <jonathan16998@gmail.com>
2018-12-30 22:38:57 +0700

    Edited util method in login service test
    Util method for generating simple employee data for login is edited to
    be more flexible.

--- service/src/test/java/com/oasis/service/test/login/LoginServiceTest.java
+++ service/src/test/java/com/oasis/service/test/login/LoginServiceTest.java
@@ -3,7 +3,6 @@ package com.oasis.service.test.login;
 import com.oasis.exception.BadRequestException;
 import com.oasis.exception.DataNotFoundException;
 import com.oasis.model.entity.EmployeeModel;
-import com.oasis.repository.EmployeeRepository;
 import com.oasis.service.api.employees.EmployeeDetailServiceApi;
 import com.oasis.service.api.employees.EmployeeUtilServiceApi;
 import com.oasis.service.implementation.login.LoginServiceImpl;
@@ -28,12 +27,17 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class LoginServiceTest {
 
+    private final String[] usernames = new String[]{ "o.s.kindy", "r.sianipar", "d.william", "a.p.lim" };
+    private final String[] names = new String[]{ "Oliver Sebastian Kindy", "Rani Sianipar", "David William",
+                                                 "Andreas Pangestu Lim" };
+    private final boolean[] deletedData = new boolean[]{ false, false, false, true };
+    private final String[] roles = new String[]{ RoleConstant.ROLE_ADMINISTRATOR, RoleConstant.ROLE_SUPERIOR,
+                                                 RoleConstant.ROLE_EMPLOYEE, RoleConstant.ROLE_EMPLOYEE };
+    private final String[] photos = new String[]{ "", "", "", "" };
     @Rule
     public ExpectedException thrown = ExpectedException.none();
     @InjectMocks
     private LoginServiceImpl loginService;
-    @Mock
-    private EmployeeRepository employeeRepository;
     @Mock
     private EmployeeDetailServiceApi employeeDetailServiceApi;
     @Mock
@@ -42,48 +46,76 @@ public class LoginServiceTest {
     private RoleDeterminer roleDeterminer;
 
     @Test
-    public void getLoginData_EmployeeWithUsernameJonathanExistsInDatabase_LoginDataIsComplete()
+    public void getLoginData_EmployeeWithUsernameExistsInDatabase_LoginDataIsComplete()
             throws
             DataNotFoundException,
             BadRequestException {
 
-        final String username = "jonathan";
-        generateEmployeeData();
+        for (int i = 0; i < usernames.length; i++) {
+            generateEmployeeDataWithoutPhoto(usernames[ i ], names[ i ], deletedData[ i ], roles[ i ]);
 
-        Map< String, String > loginData = loginService.getLoginData(username);
+            Map< String, String > loginData = loginService.getLoginData(usernames[ i ]);
 
-        assertEquals("jonathan", loginData.get("username"));
-        assertEquals("Jonathan", loginData.get("name"));
-        assertEquals(
-                "http://localhost:8085/oasis/api/employees/jonathan/photo_not_found?extension=jpg",
-                loginData.get("photo")
-        );
-        assertEquals(RoleConstant.ROLE_EMPLOYEE, loginData.get("role"));
+            assertEquals(usernames[ i ], loginData.get("username"));
+            assertEquals(names[ i ].split(" ")[ 0 ], loginData.get("name"));
+            assertEquals(
+                    "http://localhost:8085/oasis/api/employees/" + usernames[ i ] + "/photo_not_found?extension=jpg",
+                    loginData.get("photo")
+            );
+            assertEquals(roles[ i ], loginData.get("role"));
 
-        verify(employeeUtilServiceApi, times(3)).findByDeletedIsFalseAndUsername("jonathan");
-        verify(employeeDetailServiceApi, times(1)).getEmployeeDetailPhoto("jonathan", null);
-        verify(roleDeterminer, times(1)).determineRole("jonathan");
+            verify(employeeUtilServiceApi, times(3)).findByDeletedIsFalseAndUsername(usernames[ i ]);
+            verify(employeeDetailServiceApi, times(1)).getEmployeeDetailPhoto(usernames[ i ], null);
+            verify(roleDeterminer, times(1)).determineRole(usernames[ i ]);
 
-        verifyNoMoreInteractions(employeeUtilServiceApi);
-        verifyNoMoreInteractions(employeeDetailServiceApi);
-        verifyNoMoreInteractions(roleDeterminer);
+            verifyNoMoreInteractions(employeeUtilServiceApi);
+            verifyNoMoreInteractions(employeeDetailServiceApi);
+            verifyNoMoreInteractions(roleDeterminer);
+        }
     }
 
-    private void generateEmployeeData()
+    //EmployeeWithUsernameJonathanDoesNotExistInDatabase
+
+    //NoEmployeeInDatabase
+
+    private void generateEmployeeDataWithoutPhoto(
+            final String username, final String name, final boolean deleted,
+            final String role
+    )
             throws
             DataNotFoundException {
 
         EmployeeModel employee = new EmployeeModel();
 
-        employee.setUsername("jonathan");
-        employee.setName("Jonathan Wijaya");
-        employee.setDeleted(false);
+        employee.setUsername(username);
+        employee.setName(name);
+        employee.setDeleted(deleted);
 
-        when(employeeUtilServiceApi.findByDeletedIsFalseAndUsername("jonathan")).thenReturn(employee);
-        when(employeeDetailServiceApi.getEmployeeDetailPhoto("jonathan", null)).thenReturn(
-                "http://localhost:8085/oasis/api/employees/jonathan/photo_not_found?extension=jpg"
+        when(employeeUtilServiceApi.findByDeletedIsFalseAndUsername(username)).thenReturn(employee);
+        when(employeeDetailServiceApi.getEmployeeDetailPhoto(username, null)).thenReturn(
+                "http://localhost:8085/oasis/api/employees/" + username + "/photo_not_found?extension=jpg"
         );
-        when(roleDeterminer.determineRole("jonathan")).thenReturn(RoleConstant.ROLE_EMPLOYEE);
+        when(roleDeterminer.determineRole(username)).thenReturn(role);
+    }
+
+    private void generateEmployeeDataWithPhoto(
+            final String username, final String name, final boolean deleted,
+            final String photoLocation, final String extension, final String role
+    )
+            throws
+            DataNotFoundException {
+
+        EmployeeModel employee = new EmployeeModel();
+
+        employee.setUsername(username);
+        employee.setName(name);
+        employee.setDeleted(deleted);
+
+        when(employeeUtilServiceApi.findByDeletedIsFalseAndUsername(username)).thenReturn(employee);
+        when(employeeDetailServiceApi.getEmployeeDetailPhoto(username, photoLocation)).thenReturn(
+                "http://localhost:8085/oasis/api/employees/" + username + "/" + username + "?extension=" + extension
+        );
+        when(roleDeterminer.determineRole(username)).thenReturn(role);
     }
 
 }
